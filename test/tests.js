ea1227f97e2111b6b7d01ce264b5621b8d52b804
Ruben Restrepo <ruben.restrepo@auth0.com>
2021-03-23 17:21:06 -0500

    Introduce range port scanning via portscanner as an alternative to freeport

--- test/tests.js
+++ test/tests.js
@@ -4,49 +4,97 @@ var assert = require('assert');
 
 describe('http-stub', function () {
 
-  it('should work with http', function (done) {
-    var stub = HttpStub('google.com');
-
-    stub.on('request', function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello World');
-    }).on('listening', function () {
-      request.get('http://google.com', function (err, resp, body) {
-        if (err) return done(err);
-        assert.equal(body, 'Hello World');
-        stub.close();
+  describe('using default port scanner', function(done) {
+    it('should work with http', function (done) {
+      var stub = HttpStub('google.com');
+  
+      stub.on('request', function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World');
+      }).on('listening', function () {
+        request.get('http://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.equal(body, 'Hello World');
+          stub.close();
+        });
+      }).on('close', function () {
+        //it should not catch this request;
+        request.get('http://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.notEqual(body, 'Hello World');
+          done();
+        });
       });
-    }).on('close', function () {
-      //it should not catch this request;
-      request.get('http://google.com', function (err, resp, body) {
-        if (err) return done(err);
-        assert.notEqual(body, 'Hello World');
-        done();
-      });
+  
     });
-
+  
+    it('should work with https', function (done) {
+      var stub = HttpStub('google.com');
+  
+      stub.on('request', function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World');
+      }).on('listening', function () {
+        request.get('https://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.equal(body, 'Hello World');
+          stub.close();
+        });
+      }).on('close', function () {
+        request.get('http://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.notEqual(body, 'Hello World');
+          done();
+        });
+      });
+  
+    });
   });
 
-  it('should work with https', function (done) {
-    var stub = HttpStub('google.com');
-
-    stub.on('request', function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello World');
-    }).on('listening', function () {
-      request.get('https://google.com', function (err, resp, body) {
-        if (err) return done(err);
-        assert.equal(body, 'Hello World');
-        stub.close();
+  describe('using new port scanner', function(done) {
+    it('should work with http', function (done) {
+      var stub = HttpStub('google.com', { useNewPortScanner: true });
+  
+      stub.on('request', function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World');
+      }).on('listening', function () {
+        request.get('http://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.equal(body, 'Hello World');
+          stub.close();
+        });
+      }).on('close', function () {
+        //it should not catch this request;
+        request.get('http://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.notEqual(body, 'Hello World');
+          done();
+        });
       });
-    }).on('close', function () {
-      request.get('http://google.com', function (err, resp, body) {
-        if (err) return done(err);
-        assert.notEqual(body, 'Hello World');
-        done();
-      });
+  
     });
-
+  
+    it('should work with https', function (done) {
+      var stub = HttpStub('google.com', { useNewPortScanner: true });
+  
+      stub.on('request', function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World');
+      }).on('listening', function () {
+        request.get('https://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.equal(body, 'Hello World');
+          stub.close();
+        });
+      }).on('close', function () {
+        request.get('http://google.com', function (err, resp, body) {
+          if (err) return done(err);
+          assert.notEqual(body, 'Hello World');
+          done();
+        });
+      });
+  
+    });
   });
-
 });
